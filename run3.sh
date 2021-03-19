bb029ce595acbf978999273c2ba55c8588b93a24
RekGRpth <rekgrpth@gmail.com>
2021-03-19 10:21:48 +0500

    arclog

--- run3.sh
+++ run3.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 
 docker network create --attachable --opt com.docker.network.bridge.name=docker docker || echo $?
-docker volume create pg_arclog
+#docker volume create pg_arclog
 docker volume create pg_save3
 docker stop pg_save3 || echo $?
 docker rm pg_save3 || echo $?
@@ -15,9 +15,9 @@ docker run \
     --env USER_ID="$(id -u)" \
     --hostname pg_save3.docker \
     --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
-    --mount type=volume,source=pg_arclog,destination=/var/lib/postgresql/pg_arclog \
     --mount type=volume,source=pg_save3,destination=/var/lib/postgresql \
     --name pg_save3 \
     --network name=docker \
     --restart always \
     rekgrpth/pg_save runsvdir /etc/service
+#    --mount type=volume,source=pg_arclog,destination=/var/lib/postgresql/pg_arclog \
