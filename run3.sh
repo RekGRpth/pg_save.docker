1c400c4e6acdfd03d0ee12d912868db13482c9d5
RekGRpth <rekgrpth@gmail.com>
2021-04-07 08:12:10 +0500

    synchronous_standby_names

--- run3.sh
+++ run3.sh
@@ -11,6 +11,7 @@ docker run \
     --env GROUP_ID="$(id -g)" \
     --env LANG=ru_RU.UTF-8 \
     --env PRIMARY_CONNINFO="host=pg_save1.docker,pg_save2.docker,pg_save3.docker application_name=pg_save3.docker target_session_attrs=read-write" \
+    --env SYNCHRONOUS_STANDBY_NAMES='FIRST 1 ("pg_save1.docker", "pg_save2.docker", "pg_save3.docker")' \
     --env TZ=Asia/Yekaterinburg \
     --env USER_ID="$(id -u)" \
     --hostname pg_save3.docker \
