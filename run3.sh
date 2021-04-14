dc0d4f6634212b8c826f836b27c16aa2043b0078
RekGRpth <rekgrpth@gmail.com>
2021-04-14 08:54:01 +0500

    op

--- run3.sh
+++ run3.sh
@@ -1,24 +1,26 @@
 #!/bin/sh -ex
 
-docker network create --attachable --opt com.docker.network.bridge.name=docker docker || echo $?
+NAME=pg_save3
+NETWORK=docker
+docker network create --attachable --opt "com.docker.network.bridge.name=$NETWORK" "$NETWORK" || echo $?
 docker volume create pg_arclog
-docker volume create pg_save3
-docker stop pg_save3 || echo $?
-docker rm pg_save3 || echo $?
+docker volume create "$NAME"
+docker stop "$NAME" || echo $?
+docker rm "$NAME" || echo $?
 docker run \
     --detach \
     --env CLUSTER_NAME=save \
     --env GROUP_ID="$(id -g)" \
-    --env HOST_NAMES=pg_save1.docker,pg_save2.docker,pg_save3.docker \
     --env LANG=ru_RU.UTF-8 \
-    --env SYNCHRONOUS_STANDBY_NAMES='FIRST 1 ("pg_save1.docker", "pg_save2.docker", "pg_save3.docker")' \
+    --env PRIMARY_CONNINFO="host=pg_save1.$NETWORK,pg_save2.$NETWORK,pg_save3.$NETWORK application_name=$NAME.$NETWORK target_session_attrs=read-write" \
+    --env SYNCHRONOUS_STANDBY_NAMES="FIRST 1 (\"pg_save1.$NETWORK\", \"pg_save2.$NETWORK\", \"pg_save3.$NETWORK\")" \
     --env TZ=Asia/Yekaterinburg \
     --env USER_ID="$(id -u)" \
-    --hostname pg_save3.docker \
+    --hostname "$NAME.$NETWORK" \
     --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
     --mount type=volume,source=pg_arclog,destination=/var/lib/postgresql/arclog \
-    --mount type=volume,source=pg_save3,destination=/var/lib/postgresql \
-    --name pg_save3 \
-    --network name=docker \
+    --mount type="volume,source=$NAME,destination=/var/lib/postgresql" \
+    --name "$NAME" \
+    --network name="$NETWORK" \
     --restart always \
     rekgrpth/pg_save runsvdir /etc/service
